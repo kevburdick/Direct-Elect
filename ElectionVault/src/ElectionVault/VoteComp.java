722326a77a3f99881cba8ff2964ea68a1657520a
Kevin Burdick <kevburdick.hotmail.com>
2012-12-01 16:10:06 -0500

    Added code for admin use cases ,  login, add new ballot,  and add list of registered voters

--- ElectionVault/src/ElectionVault/VoteComp.java
+++ ElectionVault/src/ElectionVault/VoteComp.java
@@ -11,14 +11,21 @@ import org.apache.log4j.Logger;
 public class VoteComp {
 
    static Logger logger;
-    
+   static DataBaseProxyComp database; 
+   static MessageProxyComp msgProxy; 
+   static VoteComp voteComp;
    
-   public VoteComp() {
+   static public VoteComp getReference() {return voteComp;}; 
+   
+   public VoteComp(DataBaseProxyComp pDatabase) {
        
        logger = Logger.getLogger(VoteComp.class.getName());
        logger.warn("VoteComp()");
        
-       //FIXME this will need to handle vote requests
+       database = pDatabase;
+       //FIXME this will need to handle incoming  requests
+       //readMessage();  
+       voteComp = this;
        
    }
   
